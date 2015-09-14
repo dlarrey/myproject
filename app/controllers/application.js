2f3c03e4c8a72a9aa31bc7ca8368ba0dfdce7649
dlarrey <declarrey@gmail.com>
2015-09-13 21:36:21 -0700

    Replaced test data with a call to the real flickr

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,8 +3,58 @@ import Ember from 'ember';
 var Photo = Ember.Object.extend({
     title: '',
     username: '',
-    url: '',
+    //flickr extra data
+    owner: '',
+    //flickr url data
+    id: '',
+    farm: 0,
+    secret: '',
+    server: '',
+    url: function(){
+        return "https://farm"+this.get('farm')+
+        ".staticflickr.com/"+this.get('server')+
+        "/"+this.get('id')+"_"+this.get('secret')+"_b.jpg";
+    }.property('farm','server','id','secret'),
 });
+
+
+
+
+// var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
+//     sortProperties: ['title'],
+//     sortAscending: true,
+//     content: [],
+// });
+// 
+// var testPhotos = PhotoCollection.create();
+// var testimg1 = Photo.create({
+//     title: "Google logo",
+//     username: "google",
+//     url: "https://www.google.com/images/srpr/logo11w.png"
+// });
+// 
+// var testimg2 = Photo.create({
+//     title: "UNO logo",
+//     username: "UNO",
+//     url: "http://www.unomaha.edu/_files/images/logo-subsite-o-2.png"
+// });
+// 
+// var testimg3 = Photo.create({
+//     title: "Facebook Logo",
+//     username: "Facebook",
+//     url: "https://www.facebook.com/images/fb_icon_325x325.png"
+// });
+// 
+// var testimg4 = Photo.create({
+//     title: "Hubble Carina Nebula",
+//     username: "NASA",
+//     url: "http://imgsrc.hubblesite.org/hu/db/images/hs-2010-13-a-1920x1200_wallpaper.jpg"
+// });
+// 
+// testPhotos.pushObject(testimg1);
+// testPhotos.pushObject(testimg2);
+// testPhotos.pushObject(testimg3);
+// testPhotos.pushObject(testimg4);
 
 var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
     sortProperties: ['title'],
@@ -12,39 +62,9 @@ var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
     content: [],
 });
 
-var testPhotos = PhotoCollection.create();
-var testimg1 = Photo.create({
-    title: "Google logo",
-    username: "google",
-    url: "https://www.google.com/images/srpr/logo11w.png"
-});
-
-var testimg2 = Photo.create({
-    title: "UNO logo",
-    username: "UNO",
-    url: "http://www.unomaha.edu/_files/images/logo-subsite-o-2.png"
-});
-
-var testimg3 = Photo.create({
-    title: "Facebook Logo",
-    username: "Facebook",
-    url: "https://www.facebook.com/images/fb_icon_325x325.png"
-});
-
-var testimg4 = Photo.create({
-    title: "Hubble Carina Nebula",
-    username: "NASA",
-    url: "http://imgsrc.hubblesite.org/hu/db/images/hs-2010-13-a-1920x1200_wallpaper.jpg"
-});
-
-testPhotos.pushObject(testimg1);
-testPhotos.pushObject(testimg2);
-testPhotos.pushObject(testimg3);
-testPhotos.pushObject(testimg4);
-
 export default Ember.Controller.extend({
-    photos: testPhotos,
-    searchField: '',
+    photos: PhotoCollection.create(),
+   searchField: '',
     filteredPhotos: function () {
         var filter = this.get('searchField');
         var rx = new RegExp(filter, 'gi');
@@ -60,16 +80,32 @@ export default Ember.Controller.extend({
         },
 
         getPhotos: function(){
-            var apiKey = '1c81ab9e9be0f2efb87e2f56210406f9'; // Secret 1b6248eeaa721bbf
+            var apiKey = '1c81ab9e9be0f2efb87e2f56210406f9'; // Secret 1b6248eeaa721bbf46afdefe8cde4ac04e84904e6e10de9e';
             var host = 'https://api.flickr.com/services/rest/';
             var method = "flickr.tags.getClusterPhotos";
             var tag = "hi";
             var requestURL = host + "?method="+method + "&api_key="+apiKey+"&tag="+tag+"&format=json&nojsoncallback=1";
-            Ember.$.getJSON(requestURL, function(data){//callback for successfully completed requests
+            var photos = this.get('photos');
+            Ember.$.getJSON(requestURL, function(data){
+                //callback for successfully completed requests
                 console.log(data);
+                data.photos.photo.map(function(photo) {
+                    var newPhotoItem = Photo.create({
+                        title: photo.title,
+                        username: photo.username,
+                        //flickr extra data
+                        owner: photo.owner,
+                        //flickr url data
+                        id: photo.id,
+                        farm: photo.farm,
+                        secret: photo.secret,
+                        server: photo.server,
+                    });
+                    photos.pushObject(newPhotoItem);
+                })
             });
+        },  
 
-        },
     }
 });
 
